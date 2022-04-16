7ac0ac047812704c7d06232aa69b835cea3a946a
Brian Wilson <brian@wiltech.org>
2022-04-16 12:52:47 -0400

    doc update

--- FO4 - Brians Weapon Patcher.pas
+++ FO4 - Brians Weapon Patcher.pas
@@ -1,6 +1,27 @@
 {
-	New script template, only shows processed records
-	Assigning any nonzero value to Result will terminate script
+	MIT License
+
+	Copyright (c) 2022 Brian Wilson
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+	
+	Questions: brian@wiltech.org
 }
 unit userscript;
 
@@ -27,7 +48,7 @@ begin
 	AmmoList := TStringList.Create;
 	omodAmmoList := TStringList.Create;
 	
-	// Load values from CSV
+	// Update these values from the shell script or manualy edit them
 	AmmoList.Add('01F279=62,7,0.9,0.95,0.7,Normal formula behaviour,1');
 	AmmoList.Add('0020CB=58,7,0.89,0.95,0.7,Normal formula behaviour,1');
 	AmmoList.Add('01F278=29,4,0.8,0.95,0.7,No dismember/explode,1');
