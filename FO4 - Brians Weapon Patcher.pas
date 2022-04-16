6462f0bd3cabcd6e0f2d130dde603fa3ca735883
Brian Wilson <brian@wiltech.org>
2022-04-16 12:46:49 -0400

    Working weapon and OMOD caliber change patcher. Will now patch ALL WEAP & OMOD records you have in your load order into a new esp

--- FO4 - Brians Weapon Patcher.pas
+++ FO4 - Brians Weapon Patcher.pas
@@ -8,7 +8,7 @@ uses 'lib\mxpf';
 
 // Global variables
 var
-	AmmoList: TStringList;
+	AmmoList, omodAmmoList: TStringList;
 
 // helpers
 { Get native base (last 6) as string }
@@ -19,11 +19,13 @@ end;
 
 function Initialize: integer;
 var
-	tmp:string;
-	
+	prop, props: IInterface;
+	i: integer;
+	pfOmodAmmoChange, pfOmodDamageChange, pfOmodHasMaster: boolean;
 begin
     // Initialize stringlists
 	AmmoList := TStringList.Create;
+	omodAmmoList := TStringList.Create;
 	
 	// Load values from CSV
 	AmmoList.Add('01F279=62,7,0.9,0.95,0.7,Normal formula behaviour,1');
@@ -42,7 +44,7 @@ begin
 	AmmoList.Add('000FFA=26,4,0.7,0.95,0.7,No dismember/explode,1');
 	AmmoList.Add('0020BF=24,4,0.5,0.95,0.7,No dismember/explode,1');
 	AmmoList.Add('0020C1=27,4,0.55,0.95,0.7,No dismember/explode,1');
-	AmmoList.Add('00217B=34,4,0.6,0.95,0.7,Normal formula behaviour,1');
+	AmmoList.Add('00217B=39,4,0.6,0.95,0.7,Normal formula behaviour,1');
 	AmmoList.Add('01F673=56,6,0.35,0.95,0.7,Explode only,1');
 	AmmoList.Add('003B88=39,6,0.3,0.95,0.7,Normal formula behaviour,1');
 	AmmoList.Add('01F66B=49,7,0.8,0.95,0.7,No dismember/explode,1');
@@ -65,15 +67,164 @@ begin
 	
 	DefaultOptionsMXPF;
 	InitializeMXPF;
+	PatchFileByName('Brians_Test_Weapon_Patch');
+	
+	// WEAP Prefilter
+	LoadRecords('WEAP');
+	for i := 0 to MaxRecordIndex do
+	begin
+		if (Signature(GetRecord(i)) = 'WEAP') then
+		begin
+			PreFilterWEAPRecord(i);
+			//RemoveRecord(i);
+		end;
+	end;
+	
+	// OMOD prefilter // this still does go through all loaded records, not just OMOD. need to fix;
+	LoadRecords('OMOD');
+	BuildAmmoModCache;
+	for i := 0 to MaxRecordIndex do
+	begin
+		if (Signature(GetRecord(i)) = 'OMOD') then
+		begin
+			PreFilterOMODRecord(i);
+		end;
+	end;
+	
+	//CopyRecordsToPatch;
+	// Patch records
+	for i := 0 to MaxPatchRecordIndex do
+	begin
+		//AddMessage(Signature(GetPatchRecord(i)));
+		if (Signature(GetPatchRecord(i)) = 'WEAP') then
+		begin
+			PatchWeaponRecord(GetPatchRecord(i));
+		end;
+		if (Signature(GetPatchRecord(i)) = 'OMOD') then
+		begin
+			AddMessage('Patching OMOD: ' + Name(GetPatchRecord(i)));
+			PatchOMODRecord(i);
+		end;
+	end;
 end;
 
-// called for every record selected in xEdit
-function Process(e: IInterface): integer;
+function PreFilterWEAPRecord(var i: integer): integer;
 var
-	key: string;
-	tmpList: TStringList;
+	j: integer;
+	bBallistic, bHasAmmoData: boolean;
+	rec: IInterface;
+begin
+	Result := 0;
+	rec := GetRecord(i);
+	bBallistic := False;
+	bHasAmmoData := False;
+	
+	if (HasKeyword(rec, 'WeaponTypeBallistic')) then
+	begin
+		bBallistic := True;
+	end;
+	
+	//AddMessage('Debug: ' + Name(rec) + ' : AmmoList = ' + AmmoList.Values[genvb(rec, 'DNAM\Ammo')]);
+	if (Length(AmmoList.Values[genvb(rec, 'DNAM\Ammo')]) > 0) then
+	begin
+		bHasAmmoData := True;
+	end;
+	
+	if bBallistic and bHasAmmoData then
+	begin
+		CopyRecordToPatch(i);
+		//RemoveRecord(i);
+		AddMessage('WEAP: ' + Name(rec) + ' : ' + geev(rec, 'DNAM\Ammo'));
+	end;
+end;
+
+function BuildAmmoModCache: integer;
+var
+	tmp: string;
+	i, j: integer;
+	prop, props, rec: IInterface;
+begin
+	Result := 0;
+	for i := 0 to MaxPatchRecordIndex do
+	begin
+		rec := GetPatchRecord(i);
+		//ProcessWeapon(rec);
+	
+		// Cache the weapon ammo base form id (geevb) to the keywords starting with ma_ supplied w/ the weapon
+		// This is for OMOD patching later on...
+		props := ElementByPath(rec, 'KWDA');
+		for j := 0 to Pred(ElementCount(props)) do
+		begin
+			prop := ElementByIndex(props, j);
+			tmp := GetEditValue(prop);
+			if (pos('ma_', tmp) > 0) and (pos('ma_BallisticGun', tmp) = 0) then
+			begin
+				omodAmmoList.Values[RightStr(IntToHex(GetNativeValue(prop), 8), 6)] := genvb(rec, 'DNAM\Ammo');
+				//AddMessage('DEBUG: ' + Name(rec) + ' ' + RightStr(IntToHex(GetNativeValue(prop), 8), 6) + '=' + genvb(rec, 'DNAM\Ammo'));
+			end;
+		end;
+	end;
+end;
+
+function PreFilterOMODRecord(var i: integer): integer;
+var
+	tmp: string;
+	j: integer;
+	pfOmodAmmoChange, pfOmodDamageChange, pfOmodHasMaster: boolean;
+	prop, props: IInterface;
+begin
+	Result := 0;
+	if (geev(GetRecord(i), 'DATA\Form Type') = 'Weapon') then
+	begin
+		pfOmodAmmoChange := False;
+		pfOmodDamageChange := False;
+		pfOmodHasMaster := False;
+		
+		// Check properties
+		props := ElementByPath(GetRecord(i), 'DATA\Properties');
+		for j := 0 to Pred(ElementCount(props)) do
+		begin
+			prop := ElementByIndex(props, j);
+			if (geev(prop, 'Value Type') = 'FormID,Int') and (geev(prop, 'Property') = 'Ammo') then
+			begin
+				pfOmodAmmoChange := True;
+			end;
+			if (geev(prop, 'Value Type') = 'Float') and (geev(prop, 'Property') = 'AttackDamage') then
+			begin
+				pfOmodDamageChange := True;
+			end;
+		end;
+		
+		// Check attach point keywords...
+		props := ElementByPath(GetRecord(i), 'MNAM');
+		for j := 0 to Pred(ElementCount(props)) do
+		begin
+			prop := ElementByIndex(props, j);
+			tmp := RightStr(IntToHex(GetNativeValue(prop), 8), 6); // our ammo list key
+			//AddMessage('DEBUG: ' + GetEditValue(prop) + ' = ' + tmp + ' Parant ammo: ' + omodAmmoList.Values[tmp]);
+			if (Length(omodAmmoList.Values[tmp]) > 0) then
+			begin
+				pfOmodHasMaster := True;
+			end;
+		end;
+		
+		// If all is good, copy the OMOD record to the patch
+		if pfOmodAmmoChange and pfOmodDamageChange and pfOmodHasMaster then
+		begin
+			CopyRecordToPatch(i);
+			//RemoveRecord(i);
+			AddMessage('OMOD: ' + Name(GetRecord(i)) + ': ' + GetEditValue(prop));
+		end;
+	end;
+end;
+
+function PatchWeaponRecord(e: IInterface): integer;
+var
+	key, tmp: string;
+	tmpList, ammoCacheList: TStringList;
 	i, j: integer;
 	f: float;
+	prop, props: IInterface;
 begin
     Result := 0;
 	tmpList := TStringList.Create;
@@ -118,10 +269,63 @@ begin
 			seev(e, 'DNAM\On Hit', tmpList[5]);
 			seev(e, 'DNAM\Reload Speed', tmpList[6]);
 			
+
+			
 		//else
 		//	AddMessage('Ammo: ' + geev(e, 'DNAM\Ammo') + ' has not been registered!');
 		end;
 		
+	end;
+	tmpList.Free;
+end;
+
+function PatchOMODRecord(var i: integer): integer;
+var
+	ammoKey, attachKey, tmp: string;
+	j, k: integer;
+	f1, f2: float;
+	prop, props, rec: IInterface;
+	tmpList: TStringList;
+begin
+	Result := 0;
+	rec := GetPatchRecord(i);
+	tmpList := TStringList.Create;
+	tmpList.Delimiter := ',';
+	tmpList.StrictDelimiter := True;
+	
+	// Get keys
+	props := ElementByPath(rec, 'MNAM');
+	for j := 0 to Pred(ElementCount(props)) do
+	begin
+		prop := ElementByIndex(props, j);
+		tmp := RightStr(IntToHex(GetNativeValue(prop), 8), 6);
+		if (Length(omodAmmoList.Values[tmp]) > 0) then
+		begin
+			attachKey := tmp;
+			ammoKey := omodAmmoList.Values[attachKey];
+			//Break;
+		end;
+	end;
+	
+	tmpList.DelimitedText := AmmoList.Values[ammoKey];
+	props := ElementByPath(rec, 'DATA\Properties');
+	for j := 0 to Pred(ElementCount(props)) do
+	begin
+		prop := ElementByIndex(props, j);
+		if (geev(prop, 'Value Type') = 'Float') and (geev(prop, 'Property') = 'AttackDamage') then
+		begin
+			// Original caliber base damage
+			k := random(strtoint(tmpList[1])) + strtoint(tmpList[0]);
+			if (HasKeyword(rec, 'WeaponTypePistol')) then
+			begin
+				f1 := k;
+				k := Round(f1 * strtofloat(tmpList[3]));
+			end;
+			
+			seev(prop, 'Value Type', 'Int');
+			seev(prop, 'Function Type', 'SET');
+			seev(prop, 'Value 1', k);
+		end;
 	end;
 	tmpList.Free;
 end;
@@ -133,5 +337,6 @@ begin
     Result := 0;
 	FinalizeMXPF;
 	AmmoList.Free;
+	omodAmmoList.Free;
 end;
 end.
